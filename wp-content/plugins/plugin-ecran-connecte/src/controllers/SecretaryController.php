0d16c4999d946c0b81b5e67c52c091c440b6831f
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2025-01-09 16:37:03 +0100

    Je retire juste les commandes de debug.

--- wp-content/plugins/plugin-ecran-connecte/src/controllers/SecretaryController.php
+++ wp-content/plugins/plugin-ecran-connecte/src/controllers/SecretaryController.php
@@ -5,10 +5,6 @@ namespace Controllers;
 use Models\User;
 use Views\SecretaryView;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 /**
  * Class SecretaryController
  *
