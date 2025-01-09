74847c2f5bd3507600055ded1e6af28578580098
Manon VERHILLE <145963847+VERHILLE-Manon-2326111mv@users.noreply.github.com>
2025-01-09 15:39:25 +0100

    Suite liaison Départements - Utilisateurs - Informations : j'ai juste ajouté les menus dept pour l'administrateur dept. À merge sur la branche department.

--- wp-content/plugins/plugin-ecran-connecte/src/controllers/AdminDeptController.php
+++ wp-content/plugins/plugin-ecran-connecte/src/controllers/AdminDeptController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Models\Department;
 use Models\User;
 use Views\AdminDeptView;
 
@@ -40,12 +41,20 @@ class AdminDeptController extends UserController {
 	public function insert(){
 		$action = filter_input(INPUT_POST, 'createAdminDept');
 
+		$current_user = wp_get_current_user();
+
+		$deptModel = new Department();
+		$isAdmin = in_array('administrator', $current_user->roles);
+		$currentDept = $isAdmin ? null : $deptModel->getDepartmentUsers($current_user->ID)->getId();
+		$departments = $deptModel->getAll();
+
 		if (isset($action)) {
 
 			$login = filter_input(INPUT_POST, 'loginAdminDept');
 			$password = filter_input(INPUT_POST, 'pwdAdminDept');
 			$passwordConfirm = filter_input(INPUT_POST, 'pwdConfirmAdminDept');
 			$email = filter_input(INPUT_POST, 'emailAdminDept');
+			$deptId = $isAdmin ? filter_input(INPUT_POST, 'dept') : $currentDept;
 
 			if (is_string($login) && strlen($login) >= 4 && strlen($login) <= 25 &&
 			    is_string($password) && strlen($password) >= 8 && strlen($password) <= 25 &&
@@ -55,6 +64,7 @@ class AdminDeptController extends UserController {
 				$this->model->setPassword($password);
 				$this->model->setEmail($email);
 				$this->model->setRole('adminDept');
+				$this->model->setDeptId($deptId);
 
 				if (!$this->checkDuplicateUser($this->model) && $this->model->insert()) {
 					$this->view->displayInsertValidate();
@@ -65,7 +75,7 @@ class AdminDeptController extends UserController {
 				$this->view->displayErrorCreation();
 			}
 		}
-		return $this->view->displayFormAdminDept();
+		return $this->view->displayFormAdminDept($departments, $isAdmin, $currentDept);
 	}
 
 	/**
@@ -75,7 +85,14 @@ class AdminDeptController extends UserController {
 	 */
 	public function displayAllAdminDept() {
 		$users = $this->model->getUsersByRole('adminDept');
-		return $this->view->displayAllAdminDept($users);
+
+		$deptModel = new Department();
+		$userDeptList = array();
+		foreach ($users as $user) {
+			$userDeptList[] = $deptModel->getUserInDept($user->getId())->getName();
+		}
+
+		return $this->view->displayAllAdminDept($users, $userDeptList);
 	}
 
 
